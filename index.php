d8f9319720883a2f1cf9a31444726cea647d39fb
Armando <amezal01@hotmail.com>
2022-11-12 16:02:59 -0600

    added routing

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
-  $page_title = "Kermesse Project"
-?>
 
-<?php include('./partials/_nav.php') ?>
-<main>
-  <div class="container-fluid px-4">
-    <h1 class="mt-4"><?php echo($page_title) ?></h1>
-  </div>
-</main>
-<?php include('./partials/_footer.php') ?>
+$base_url = '/' . basename(__DIR__);
+$req_url = $_SERVER['REQUEST_URI'];
+$url= str_replace($base_url, '', $req_url);
+
+switch ($url) {
+  case "/":
+    require __DIR__ . '/vistas/index.php';
+    break;
+}
---
+++ vistas/index.php
@@ -0,0 +1,12 @@
+<?php
+  $page_title = 'Kermesse Project';
+?>
+
+<?php include('./partials/_nav.php') ?>
+<main>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo($page_title) ?></h1>
+    <h2><?php echo(__DIR__) ?></h2>
+  </div>
+</main>
+<?php include('./partials/_footer.php') ?>
