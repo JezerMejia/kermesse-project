6a191d2e88e9ae3178e0236c93d72b6f28d33ac4
Jezer Mejía <amyuki4@icloud.com>
2022-11-13 15:56:14 -0600

    Added rol mostrar.php fields

--- vistas/rol/mostrar.php
+++ vistas/rol/mostrar.php
@@ -1,10 +1,21 @@
 <?php
 include('entidades/rol.php');
-$page_title = 'Rol';
-$encabezados = [];
+$page_title = 'Roles';
+$encabezados = ['ID', 'Descripción', 'Estado'];
 $campo_id = 'id_rol';
-$campos = ['id_rol'];
-$datos = []
+$campos = ['id_rol', 'descripcion', 'estado'];
+
+$rol1 = new Rol();
+$rol1->__SET('id_rol', 1);
+$rol1->__SET('descripcion', 'Administrador');
+$rol1->__SET('estado', 0);
+
+$rol2 = new Rol();
+$rol2->__SET('id_rol', 2);
+$rol2->__SET('descripcion', 'Usuario');
+$rol2->__SET('estado', 0);
+
+$datos = [$rol1, $rol2];
 ?>
 
 <?php include('./partials/_nav.php') ?>
