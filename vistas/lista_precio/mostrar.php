1e1504fc4aceec84d48e56aa0c7bbf343821f668
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-13 17:59:00 -0600

    Added a little change in ingreso_comunidad_det

--- vistas/lista_precio/mostrar.php
+++ vistas/lista_precio/mostrar.php
@@ -1,10 +1,18 @@
 <?php
 include('entidades/lista_precio.php');
 $page_title = 'Lista Precio';
-$encabezados = [];
+$encabezados = ['ID', 'ID Kermesse','Nombre','Descripcion','estado','Opciones'];
 $campo_id = 'id_lista_precio';
-$campos = ['id_lista_precio'];
-$datos = []
+$campos = ['id_lista_precio', 'id_kermesse','nombre','descripcion','estado'];
+
+$lp = new ListaPrecio();
+$lp-> __SET('id_lista_precio',1);
+$lp-> __SET('id_kermesse',1);
+$lp-> __SET('nombre',"Juan");
+$lp-> __SET('descripcion',"Lorem ipsum sadlad is navil santus patronus");
+$lp-> __SET('estado',1);
+
+$datos = [$lp]
 ?>
 
 <?php include('./partials/_nav.php') ?>
