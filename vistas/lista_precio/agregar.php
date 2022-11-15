af8623c47be8d964ba9db091599b8caf9b23e5be
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-14 22:37:31 -0600

    Added a form to lista_precio vista

--- vistas/lista_precio/agregar.php
+++ vistas/lista_precio/agregar.php
@@ -11,7 +11,52 @@ $datos = []
 <main>
 <div class="container-fluid px-4">
   <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
-  
+  <div class="card mb-4 w-100 p-1">
+      <div class="card-body">
+        En este formulario se agregaran datos a lista precio.
+      </div>
+
+      <div class="card mb-4">
+        <div class="card-body">
+          <!---->
+          <form>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">ID</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+                type="id" placeholder="ID" disabled />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">ID Kermesse</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1" 
+                type="id_kermesse" placeholder="ID Kermesse" disabled />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">Nombre</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+              type="nombre" placeholder="Nombre" />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">Descripcion</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+              type="descripcion" placeholder="Descripcion"/>
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">Estado</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+              type="estado" placeholder="Estado"/>
+            </div>
+            
+            <button class="btn btn-primary" type="button">Agregar</button>
+
+          </form>
+          <!---->
+        </div>
+      </div>
+    </div>
 </div>
 </main>
 <?php include('./partials/_footer.php') ?>
