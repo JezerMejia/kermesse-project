e5b33bf05a613f5c0ef342757030b948e784e502
Jezer Mejía <amyuki4@icloud.com>
2022-11-14 18:24:09 -0600

    Added connection data class

---
+++ datos/connection.php
@@ -0,0 +1,40 @@
+<?php
+
+class Connection {
+
+  private $pdo;
+  private $pdo_stmt;
+  private $server_name;
+  private $db_name;
+  private $user_name;
+  private $pwd;
+
+  public function connect() {
+    $server_name = '0.tcp.sa.ngrok.io:18064';
+    $db_name = 'dbkermesse';
+    $user_name = 'root';
+    $pwd = 'Usuario123.';
+
+    try {
+      $this->pdo = new PDO("mysql:host={$server_name};dbname={$db_name}",$user_name,$pwd);
+      $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+      return $this->pdo;
+    } catch(PDOException $e) {
+      die($e->getMessage());
+    }
+  }
+
+  public function disconnect() {
+    try {
+      $pdo = null;
+      return $pdo;
+    } catch(PDOException $e) {
+      die($e->getMessage());
+    }
+  }
+}
+
+$con = new Connection();
+$con->connect();
+$con->disconnect();
