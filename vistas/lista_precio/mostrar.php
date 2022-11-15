7bb44d4a8dd5e0a22513059bac05d84627e5cd9b
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-15 12:28:01 -0600

    Fixed a little mistake

--- vistas/lista_precio/mostrar.php
+++ vistas/lista_precio/mostrar.php
@@ -1,14 +1,21 @@
 <?php
 include('entidades/lista_precio.php');
 include('datos/dt_lista_precio.php');
+include('datos/dt_kermesse.php');
 $page_title = 'Lista Precio';
 $encabezados = ['ID', 'ID Kermesse','Nombre','Descripcion','estado','Opciones'];
 $campo_id = 'id_lista_precio';
 $campos = ['id_lista_precio', 'id_kermesse','nombre','descripcion','estado'];
 
 $lp = new DtListaPrecio();
+$dt_kermesse = new DtKermesse();
 
 $datos = $lp->get_data();
+
+foreach($datos as $lp) {
+  $kermesse = $dt_kermesse->find_by_id($lp->__GET('id_kermesse'));
+  $lp->__SET('kermesse', $kermesse->__GET('nombre'));
+}
 ?>
 
 <?php include('./partials/_nav.php') ?>
