bacd8349a56dbf56599555e1255f4689fdcf475e
Roire <roirevh@gmail.com>
2022-11-15 10:42:50 -0600

    Formularios parroquia, moneda y comunidad

--- vistas/moneda/agregar.php
+++ vistas/moneda/agregar.php
@@ -1,17 +1,46 @@
 <?php
-include('entidades/moneda.php');
+include('datos/dt_moneda.php');
 $page_title = 'Agregar Moneda';
-$encabezados = [];
-$campo_id = 'id_moneda';
-$campos = ['id_moneda'];
-$datos = []
+$dt_moneda = new DtMoneda();
+$monedas = $dt_moneda->get_data();
 ?>
 
 <?php include('./partials/_nav.php') ?>
 <main>
-<div class="container-fluid px-4">
-  <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
-  
-</div>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
+    <div class="card mb-4 w-100 p-2">
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label>ID</label>
+            <input class="form-control form-control-solid" type="number" placeholder="ID" disabled />
+          </div>
+
+          <div class="mb-3">
+            <label>Nombre</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <label>Simbolo</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <div class="form-check">
+              <label class="form-check-label" for="estado">Activo?</label>
+              <input class="form-check-input" id="estado" type="checkbox" value="">
+            </div>
+          </div>
+
+          <div class="mt-4 d-flex gap-3">
+            <button class="btn btn-primary" type="button">Agregar</button>
+            <button class="btn btn-secondary" type="button">Cancelar</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
 </main>
-<?php include('./partials/_footer.php') ?>
--- vistas/parroquia/agregar.php
+++ vistas/parroquia/agregar.php
@@ -1,11 +1,62 @@
 <?php
-include('entidades/parroquia.php');
+include('datos/dt_parroquia.php');
 $page_title = 'Agregar Parroquia';
-$encabezados = [];
-$campo_id = 'id_parroquia';
-$campos = ['id_parroquia'];
-$datos = []
+$dt_parroquia = new DtParroquia();
+$parroquias = $dt_parroquia->get_data();
 ?>
+
+<?php include('./partials/_nav.php') ?>
+<main>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
+    <div class="card mb-4 w-100 p-2">
+      <div class="card-body">
+        <form>
+          <div class="mb-3">
+            <label>ID</label>
+            <input class="form-control form-control-solid" type="number" placeholder="ID" disabled />
+          </div>
+
+          <div class="mb-3">
+            <label>Nombre</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <label>Direccion</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <label>Telefono</label>
+            <input class="form-control" type="number">
+          </div>
+
+          <div class="mb-3">
+            <label>Parroco</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <label>Logo</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mb-3">
+            <label>Sitio Web</label>
+            <input class="form-control" type="text">
+          </div>
+
+          <div class="mt-4 d-flex gap-3">
+            <button class="btn btn-primary" type="button">Agregar</button>
+            <button class="btn btn-secondary" type="button">Cancelar</button>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div>
+</main>
 
 <?php include('./partials/_nav.php') ?>
 <main>
