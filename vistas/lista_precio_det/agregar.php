4b4cd1ffb2ba4a74c419fd2c0985793fcbfa751d
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-14 22:44:04 -0600

    Added a form to lista_precio_det vista

--- vistas/lista_precio_det/agregar.php
+++ vistas/lista_precio_det/agregar.php
@@ -9,9 +9,47 @@ $datos = []
 
 <?php include('./partials/_nav.php') ?>
 <main>
-<div class="container-fluid px-4">
-  <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
-  
-</div>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
+    <div class="card mb-4 w-100 p-1">
+      <div class="card-body">
+        En este formulario se agregaran datos a lista precio.
+      </div>
+
+      <div class="card mb-4">
+        <div class="card-body">
+          <!---->
+          <form>
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">ID</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="id" placeholder="ID" disabled />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">ID Lista precio</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+                type="id_lista_precio" placeholder="ID Lista precio" disabled />
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">ID Producto</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+                type="id_producto" placeholder="ID Producto" disabled/>
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleFormControlInput1">Precio venta</label>
+              <input class="form-control form-control-solid" id="exampleFormControlInput1"
+                type="precio_venta" placeholder="Precio venta" />
+            </div>
+
+            <button class="btn btn-primary" type="button">Agregar</button>
+
+          </form>
+          <!---->
+        </div>
+      </div>
+    </div>
+  </div>
 </main>
 <?php include('./partials/_footer.php') ?>
