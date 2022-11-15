5cef49c16e80a1658d2540f1dda684572f290b98
Roire <roirevh@gmail.com>
2022-11-14 23:23:31 -0600

    Datos comunidad

--- vistas/comunidad/mostrar.php
+++ vistas/comunidad/mostrar.php
@@ -1,25 +1,13 @@
 <?php
 include('entidades/comunidad.php');
+include('datos/dt_comunidad.php');
 $page_title = 'Comunidad';
 $encabezados = ['ID', 'Nombre', 'Responsable', 'Contribución', 'Estado', 'Opciones'];
 $campo_id = 'id_comunidad';
 $campos = ['id_comunidad', 'nombre', 'responsable','desc_contribucion','estado'];
 
-$tc = new Comunidad();
-$tc->__SET('id_comunidad',1);
-$tc->__SET('nombre', "Reparto Shick");
-$tc->__SET('responsable', "Armando Meza");
-$tc->__SET('desc_contribucion',30);
-$tc->__SET('estado',1);
-
-$tcx = new Comunidad();
-$tcx->__SET('id_comunidad',2);
-$tcx->__SET('nombre', "Barrio Domitila Lugo");
-$tcx->__SET('responsable', "Luis Fonsi");
-$tcx->__SET('desc_contribucion',50);
-$tcx->__SET('estado',1);
-
-$datos = [$tc, $tcx];
+$dt_comunidad = new DtComunidad();
+$datos = $dt_comunidad->get_data()
 ?>
 
 <?php include('./partials/_nav.php') ?>
