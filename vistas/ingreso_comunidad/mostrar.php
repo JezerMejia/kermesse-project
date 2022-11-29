70dd2b529c75f34266f89dbf8f747bf50899582a
Jezer Mejía <amyuki4@icloud.com>
2022-11-29 16:43:49 -0600

    Parse ingreso_comunidad data

--- vistas/ingreso_comunidad/mostrar.php
+++ vistas/ingreso_comunidad/mostrar.php
@@ -1,14 +1,32 @@
 <?php
-include('entidades/ingreso_comunidad.php');
-include('datos/dt_ingreso_comunidad.php');
+include_once('entidades/ingreso_comunidad.php');
+include_once('datos/dt_ingreso_comunidad.php');
+include_once('datos/dt_kermesse.php');
+include_once('datos/dt_comunidad.php');
+include_once('datos/dt_producto.php');
+
 $page_title = 'Ingreso Comunidad';
-$encabezados = ['ID', 'ID Kermesse', 'ID Comunidad', 'ID Producto', 'Cantidad de productos', 'Total Bonos', 'estado','Opciones'];
+$encabezados = ['ID', 'Kermesse', 'Comunidad', 'Producto', 'Cantidad de productos', 'Total Bonos', 'Estado','Opciones'];
 $campo_id = 'id_ingreso_comunidad';
-$campos = ['id_ingreso_comunidad','id_kermesse','id_comunidad','id_producto','cant_productos','total_bonos',
+$campos = ['id_ingreso_comunidad','kermesse','comunidad','producto','cant_productos','total_bonos',
 'estado'];
 
-$ic = new DtIngresoComunidad();
-$datos = $ic->get_data();
+$dt_ingreso = new DtIngresoComunidad();
+$dt_kermesse = new DtKermesse();
+$dt_comunidad = new DtComunidad();
+$dt_producto = new DtProducto();
+
+$datos = $dt_ingreso->get_data();
+
+foreach ($datos as $dato) {
+  $ker = $dt_kermesse->find_by_id($dato->__GET('id_kermesse'));
+  $com = $dt_comunidad->find_by_id($dato->__GET('id_comunidad'));
+  $prod = $dt_producto->find_by_id($dato->__GET('id_producto'));
+
+  $dato->__SET('kermesse', $ker->__GET('nombre'));
+  $dato->__SET('comunidad', $com->__GET('nombre'));
+  $dato->__SET('producto', $prod->__GET('nombre'));
+}
 ?>
 
 <?php include('./partials/_nav.php') ?>
