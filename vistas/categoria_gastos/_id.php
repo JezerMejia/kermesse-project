d09bc0c3dcda488c2231f8e95c2400da4b9a6b83
Jezer Mejía <amyuki4@icloud.com>
2022-11-29 04:09:12 -0600

    Added Details view

--- vistas/categoria_gastos/_id.php
+++ vistas/categoria_gastos/_id.php
@@ -1,17 +1,42 @@
 <?php
 include('entidades/categoria_gastos.php');
-$page_title = 'Categoria Gastos';
-$encabezados = [];
-$campo_id = 'id_categoria_gastos';
-$campos = ['id_categoria_gastos'];
-$datos = []
+include('datos/dt_categoria_gastos.php');
+
+$dt_cat_gastos = new DtCategoriaGastos();
+
+$cat_gasto = $dt_cat_gastos->find_by_id($id_from_url);
+
+$page_title = 'Categoria de Gastos';
 ?>
 
 <?php include('./partials/_nav.php') ?>
 <main>
 <div class="container-fluid px-4">
-  <h1 class="mt-4"><?php echo ("$page_title - $id_from_url") ?></h1>
-  
-</div>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
+    <div class="card mb-4 w-100 p-2">
+      <div class="card-body">
+        <form action="<?php echo $base_url?>/negocio/ng_categoria_gastos.php" method="POST">
+          <div class="mb-3">
+            <label>ID</label>
+            <input class="form-control" readonly type="number" name="id_categoria_gastos"
+            value="<?php echo $cat_gasto->__GET('id_categoria_gastos')?>"/>
+          </div>
+
+          <div class="mb-3">
+            <label>Nombre</label>
+            <input class="form-control" readonly type="text" required name="nombre_categoria"
+              value="<?php echo $cat_gasto->__GET('nombre_categoria')?>">
+          </div>
+
+          <div class="mb-3">
+            <label>Descripción</label>
+            <input class="form-control" readonly type="text" name="descripcion"
+              value="<?php echo $cat_gasto->__GET('descripcion')?>">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </main>
 <?php include('./partials/_footer.php') ?>
--- vistas/gasto/_id.php
+++ vistas/gasto/_id.php
@@ -1,17 +1,68 @@
 <?php
 include('entidades/gasto.php');
+include('datos/dt_gasto.php');
+include('datos/dt_categoria_gastos.php');
+include('datos/dt_kermesse.php');
+
+$dt_gasto = new DtGasto();
+$dt_kermesse = new DtKermesse();
+$dt_cat_gasto = new DtCategoriaGastos();
+
+$gasto = $dt_gasto->find_by_id($id_from_url);
+$id_kermesse = $gasto->__GET("id_kermesse");
+$id_categoria = $gasto->__GET("id_cat_gastos");
+
+$kermesse = $dt_kermesse->find_by_id($id_kermesse);
+$categoria = $dt_cat_gasto->find_by_id($id_categoria);
+
 $page_title = 'Gasto';
-$encabezados = [];
-$campo_id = 'id_gasto';
-$campos = ['id_gasto'];
-$datos = []
 ?>
 
 <?php include('./partials/_nav.php') ?>
 <main>
-<div class="container-fluid px-4">
-  <h1 class="mt-4"><?php echo ("$page_title - $id_from_url") ?></h1>
-  
-</div>
+  <div class="container-fluid px-4">
+    <h1 class="mt-4"><?php echo ("$page_title") ?></h1>
+    <div class="card mb-4 w-100 p-2">
+      <div class="card-body">
+      <form>
+          <div class="mb-3">
+            <label>ID</label>
+            <input class="form-control" readonly type="number" name="id_gasto"
+              value="<?php echo $gasto->__GET('id_gasto')?>"/>
+          </div>
+
+          <div class="mb-3">
+            <label>Kermesse</label>
+            <input class="form-control" readonly type="text" name="id_kermesse"
+              value="<?php echo $kermesse->__GET('nombre')?>"/>
+          </div>
+
+          <div class="mb-3">
+            <label>Categoría</label>
+            <input class="form-control" readonly type="text" name="id_cat_gastos"
+              value="<?php echo $categoria->__GET('descripcion')?>"/>
+          </div>
+
+          <div class="mb-3">
+            <label>Fecha Gasto</label>
+            <input class="form-control" readonly type="date" name="fecha_gasto"
+              value="<?php echo $gasto->__GET('fecha_gasto')?>">
+          </div>
+
+          <div class="mb-3">
+            <label>Concepto</label>
+            <input class="form-control" readonly type="text" name="concepto"
+              value="<?php echo $gasto->__GET('concepto')?>">
+          </div>
+
+          <div class="mb-3">
+            <label>Monto</label>
+            <input class="form-control" readonly type="number" name="monto"
+              value="<?php echo $gasto->__GET('monto')?>">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </main>
 <?php include('./partials/_footer.php') ?>
