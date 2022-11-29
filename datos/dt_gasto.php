c2578b3cc5dd0252a425fe577c677c25d665b9d8
Jezer Mejía <amyuki4@icloud.com>
2022-11-28 23:16:17 -0600

    Fix include in dt_gasto.php

--- datos/dt_gasto.php
+++ datos/dt_gasto.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once "data_table_template.php";
-include_once "../entidades/gasto.php";
+include_once dirname(__DIR__) . "/datos/data_table_template.php";
+include_once dirname(__DIR__) . "/entidades/gasto.php";
 
 class DtGasto extends DataTableTemplate {
 
