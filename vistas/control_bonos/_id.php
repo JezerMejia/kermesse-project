470aeca8678e22c87ebcf7e704e0b66e83e70577
LncN1939 <leo.corea13419@est.uca.edu.ni>
2022-11-29 02:03:31 -0600

    Added a control_bonos id and editar connection database

--- vistas/control_bonos/_id.php
+++ vistas/control_bonos/_id.php
@@ -1,10 +1,16 @@
 <?php
 include('entidades/control_bonos.php');
-$page_title = 'Control Bonos';
+include('datos/dt_control_bonos.php');
+/*$page_title = 'Control Bonos';
 $encabezados = [];
 $campo_id = 'id_control_bonos';
 $campos = ['id_control_bonos'];
 $datos = []
+*/
+$dt_control_bonos = new DtControlBonos();
+$control_bonos = $dt_control_bonos->find_by_id($id_from_url);
+
+$page_title = 'Editar Control Bonos';
 ?>
 
 <?php include('./partials/_nav.php') ?>
@@ -23,17 +29,20 @@ $datos = []
           <form>
             <div class="mb-3">
               <label for="exampleFormControlInput1">ID</label>
-              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="id" placeholder="ID" disabled />
+              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="id" placeholder="ID"
+              value="<?php echo $control_bonos->__GET('id_bono') ?>" disabled />
             </div>
 
             <div class="mb-3">
               <label for="exampleFormControlInput1">Nombre</label>
-              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="nombre" placeholder="Nombre del usuario" disabled />
+              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="nombre" placeholder="Nombre del usuario"
+              value="<?php echo $control_bonos->__GET('nombre') ?>" disabled />
             </div>
 
             <div class="mb-3">
               <label for="exampleFormControlInput1">Valor</label>
-              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="valor" placeholder="Valor" disabled />
+              <input class="form-control form-control-solid" id="exampleFormControlInput1" type="valor" placeholder="Valor"
+              value="<?php echo $control_bonos->__GET('valor') ?>" disabled />
             </div>
           </form>
           <!---->
